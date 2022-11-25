35890a7f94bacca181599ff2a375d5057d9faaaa
evans ochieng <evansochieng121@gmail.com>
2022-11-25 14:25:01 +0300

    practice rendering associated data

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        #render json: items, except: [:created_at, :updated_at], include: :user, except: [:created_at, :updated_at]
+        render json: items, except: [:created_at, :updated_at], include: {user: {except: [:created_at, :updated_at]}}
+    end
 end
